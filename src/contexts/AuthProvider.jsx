ed2b952c119140fda78004c8a85b41a15a4a0706
Dipok Kumar Ray <dipok7316@gmail.com>
2025-06-04 20:24:53 +0600

    Added firebase hide credential implementation

---
+++ src/contexts/AuthProvider.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { AuthContext } from './AuthContext';
+
+const AuthProvider = ({children}) => {
+
+
+    const userInfo = {
+
+    }
+
+
+    return <AuthContext userInfo={userInfo}>
+        {children}
+        </AuthContext>
+};
+
+export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,16 @@ import {
   RouterProvider,
 } from "react-router";
 import router from './router/router';
+import AuthProvider from './contexts/AuthProvider';
 
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+   <AuthProvider>
     <RouterProvider router={router}>
-
     </RouterProvider>
+   </AuthProvider>
   </StrictMode>,
 )
