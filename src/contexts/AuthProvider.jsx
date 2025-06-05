62542dd8a16b6b7933fd28e725407a4864af2be4
Dipok Kumar Ray <dipok7316@gmail.com>
2025-06-05 07:28:07 +0600

    Added Login and Register AuthProvider implementation

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import { useState } from 'react';
 import { AuthContext } from './AuthContext';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { auth } from '../firebase/firebase';
+
+
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({children}) => {
+const [user, setUser] = useState(null);
+const [loading, setLoading] = useState(true);
+
+    //google sign in
+    const signInWithGoogle = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider)
+    }
+
+    //register login user
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
 
 
     const userInfo = {
+        user,
+        loading,
+        signInWithGoogle,
+        createUser,
 
     }
 
--- src/shared/Login.jsx
+++ src/shared/Login.jsx
@@ -1,11 +1,71 @@
-import React from 'react';
+// import React, { useContext } from "react";
+// import { AuthContext } from "../../contexts/AuthContext";
+// import loginLottie from '../../assets/lotties/login.json'
+// import Lottie from "lottie-react";
+// import SocialLogin from "./SocialLogin";
+// import { useLocation, useNavigate } from "react-router";
+
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import { useLocation, useNavigate } from "react-router";
+import SocialLogin from "./SocialLogin";
 
 const Login = () => {
-    return (
-        <div>
-            <h2>Login ........</h2>
+
+  const {userLogin} = useContext(AuthContext);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const from = location.state || '/';
+
+  console.log('Loacation in LogIn Page', location);
+  
+      const handleLogin = e => {
+          e.preventDefault();
+          const form = e.target;
+          const email = form.email.value;
+          const password = form.password.value;
+          console.log(email, password);
+  
+  
+          //create user
+          userLogin(email, password)
+          .then(result => {
+              console.log(result.user);
+              navigate(from);
+          })
+          .catch(error => {
+              console.log(error.message);
+          })
+  
+      }
+   
+  return (
+  <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        {/* <div className="text-center lg:text-left">
+            <Lottie style={{width: '250px'}} animationData={loginLottie} loop={true}></Lottie>
+        </div> */}
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <div className="card-body">
+          <h1 className="text-3xl font-bold">Login now!</h1>
+          <form onSubmit={handleLogin}>
+               <fieldset className="fieldset">
+              <label className="label">Email</label>
+              <input type="email" className="input" name="email" placeholder="Email" required />
+              <label className="label">Password</label>
+              <input type="password" className="input" name="password" placeholder="Password"  required/>
+              <div>
+                <a className="link link-hover">Forgot password?</a>
+              </div>
+              <button className="btn btn-neutral mt-4">Login</button>
+            </fieldset>
+          </form>
+          <SocialLogin from={from}/>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
